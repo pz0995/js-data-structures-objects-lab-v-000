a879197f039053a3788da9a416dc4e7d77df2e7f
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-02 00:36:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+
+function deleteFromDriverByKey() {
+  
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
